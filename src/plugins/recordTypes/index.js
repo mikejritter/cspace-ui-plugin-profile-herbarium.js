9b15da07aafb1d1760077088cf68e44182a397fc
Ray Lee <ray.lee@lyrasis.org>
2020-01-09 12:39:16 -0800

    DRYD-745: Disable unused record types.

--- src/plugins/recordTypes/index.js
+++ src/plugins/recordTypes/index.js
@@ -1,13 +1,19 @@
+import acquisition from './acquisition';
 import collectionobject from './collectionobject';
 import concept from './concept';
+import conservation from './conservation';
 import loanin from './loanin';
 import loanout from './loanout';
+import objectexit from './objectexit';
 import taxon from './taxon';
 
 export default [
+  acquisition,
   collectionobject,
   concept,
+  conservation,
   loanin,
   loanout,
+  objectexit,
   taxon,
 ];
