1a1c45ffa1d401370ccfceffb3f4eec32ea267ee
Michael Ritter <emericaskate15@gmail.com>
2022-10-25 12:40:33 -0600

    DRYD-1026: Disable public browser template

--- src/plugins/recordTypes/collectionobject/forms/index.js
+++ src/plugins/recordTypes/collectionobject/forms/index.js
@@ -2,6 +2,9 @@ import defaultForm from './default';
 
 export default (configContext) => ({
   default: defaultForm(configContext),
+  public: {
+    disabled: true,
+  },
   timebased: {
     disabled: true,
   },
