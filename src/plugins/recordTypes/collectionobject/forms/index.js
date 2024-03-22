c5306435f6a969f0ad971b4c9893c80f3aaa61a3
Michael Ritter <mikejritter@users.noreply.github.com>
2024-03-21 19:38:45 -0600

    Remove objectName and material groups from templates (#50)

--- src/plugins/recordTypes/collectionobject/forms/index.js
+++ src/plugins/recordTypes/collectionobject/forms/index.js
@@ -1,7 +1,11 @@
 import defaultForm from './default';
+import inventoryForm from './inventory';
+import photoForm from './photo';
 
 export default (configContext) => ({
   default: defaultForm(configContext),
+  inventory: inventoryForm(configContext),
+  photo: photoForm(configContext),
   public: {
     disabled: true,
   },
---
+++ src/plugins/recordTypes/collectionobject/forms/photo.jsx
@@ -0,0 +1,249 @@
+const template = (configContext) => {
+  const {
+    React,
+  } = configContext.lib;
+
+  const {
+    Col,
+    Panel,
+    Row,
+  } = configContext.layoutComponents;
+
+  const {
+    Field,
+  } = configContext.recordComponents;
+
+  const {
+    extensions,
+  } = configContext.config;
+
+  return (
+    <Field name="document">
+      <Panel name="id" collapsible>
+        <Row>
+          <Col>
+            <Field name="objectNumber" />
+
+            <Field name="responsibleDepartments">
+              <Field name="responsibleDepartment" />
+            </Field>
+
+            <Row>
+              <Field name="collection" />
+
+              <Col>
+                <Field name="namedCollections">
+                  <Field name="namedCollection" />
+                </Field>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col>
+            <Field name="briefDescriptions">
+              <Field name="briefDescription" />
+            </Field>
+
+            <Field name="distinguishingFeatures" />
+          </Col>
+        </Row>
+
+        <Field name="titleGroupList">
+          <Field name="titleGroup">
+            <Panel>
+              <Row>
+                <Col>
+                  <Field name="title" />
+                  <Field name="titleLanguage" />
+                </Col>
+
+                <Col>
+                  <Field name="titleType" />
+
+                  <Field name="titleTranslationSubGroupList">
+                    <Field name="titleTranslationSubGroup">
+                      <Field name="titleTranslation" />
+                      <Field name="titleTranslationLanguage" />
+                    </Field>
+                  </Field>
+                </Col>
+              </Row>
+            </Panel>
+          </Field>
+        </Field>
+
+        <Field name="objectCountGroupList">
+          <Field name="objectCountGroup">
+            <Field name="objectCount" />
+            <Field name="objectCountType" />
+            <Field name="objectCountCountedBy" />
+            <Field name="objectCountDate" />
+            <Field name="objectCountNote" />
+          </Field>
+        </Field>
+      </Panel>
+
+      <Panel name="desc" collapsible collapsed>
+        <Row>
+          <Col>
+            <Field name="copyNumber" />
+          </Col>
+
+          <Col>
+            <Field name="editionNumber" />
+
+            <Field name="colors">
+              <Field name="color" />
+            </Field>
+          </Col>
+        </Row>
+
+        <Field name="physicalDescription" />
+
+        <Row>
+          <Field name="technicalAttributeGroupList">
+            <Field name="technicalAttributeGroup">
+              <Field name="technicalAttribute" />
+              <Field name="technicalAttributeMeasurement" />
+              <Field name="technicalAttributeMeasurementUnit" />
+            </Field>
+          </Field>
+
+          <Col />
+        </Row>
+
+        {extensions.dimension.form}
+
+        <Panel name="content" collapsible collapsed>
+          <Field name="contentDescription" />
+
+          <Row>
+            <Col>
+              <Field name="contentLanguages">
+                <Field name="contentLanguage" />
+              </Field>
+
+              <Field name="contentActivities">
+                <Field name="contentActivity" />
+              </Field>
+
+              <Field name="contentConcepts">
+                <Field name="contentConcept" />
+              </Field>
+
+              <Field name="contentDateGroup" />
+
+              <Field name="contentPositions">
+                <Field name="contentPosition" />
+              </Field>
+
+              <Field name="contentObjectGroupList">
+                <Field name="contentObjectGroup">
+                  <Field name="contentObject" />
+                  <Field name="contentObjectType" />
+                </Field>
+              </Field>
+            </Col>
+
+            <Col>
+              <Field name="contentPeoples">
+                <Field name="contentPeople" />
+              </Field>
+
+              <Field name="contentPersons">
+                <Field name="contentPerson" />
+              </Field>
+
+              <Field name="contentPlaces">
+                <Field name="contentPlace" />
+              </Field>
+
+              <Field name="contentScripts">
+                <Field name="contentScript" />
+              </Field>
+
+              <Field name="contentOrganizations">
+                <Field name="contentOrganization" />
+              </Field>
+
+              <Field name="contentEventNameGroupList">
+                <Field name="contentEventNameGroup">
+                  <Field name="contentEventName" />
+                  <Field name="contentEventNameType" />
+                </Field>
+              </Field>
+
+              <Field name="contentOtherGroupList">
+                <Field name="contentOtherGroup">
+                  <Field name="contentOther" />
+                  <Field name="contentOtherType" />
+                </Field>
+              </Field>
+            </Col>
+          </Row>
+
+          <Field name="contentNote" />
+        </Panel>
+      </Panel>
+
+      <Panel name="prod" collapsible collapsed>
+        <Row>
+          <Col>
+            <Field name="objectProductionDateGroupList">
+              <Field name="objectProductionDateGroup" />
+            </Field>
+
+            <Field name="techniqueGroupList">
+              <Field name="techniqueGroup">
+                <Field name="technique" />
+                <Field name="techniqueType" />
+              </Field>
+            </Field>
+
+            <Field name="objectProductionPlaceGroupList">
+              <Field name="objectProductionPlaceGroup">
+                <Field name="objectProductionPlace" />
+                <Field name="objectProductionPlaceRole" />
+              </Field>
+            </Field>
+          </Col>
+
+          <Col>
+            <Field name="objectProductionPersonGroupList">
+              <Field name="objectProductionPersonGroup">
+                <Field name="objectProductionPerson" />
+                <Field name="objectProductionPersonRole" />
+              </Field>
+            </Field>
+
+            <Field name="objectProductionOrganizationGroupList">
+              <Field name="objectProductionOrganizationGroup">
+                <Field name="objectProductionOrganization" />
+                <Field name="objectProductionOrganizationRole" />
+              </Field>
+            </Field>
+
+            <Field name="objectProductionNote" />
+          </Col>
+        </Row>
+      </Panel>
+
+      <Panel name="reference" collapsible collapsed>
+        <Field name="referenceGroupList">
+          <Field name="referenceGroup">
+            <Field name="reference" />
+            <Field name="referenceNote" />
+          </Field>
+        </Field>
+      </Panel>
+
+      <Panel name="hierarchy" collapsible collapsed>
+        <Field name="relation-list-item" subpath="rel:relations-common-list" />
+      </Panel>
+    </Field>
+  );
+};
+
+export default (configContext) => ({
+  template: template(configContext),
+});
