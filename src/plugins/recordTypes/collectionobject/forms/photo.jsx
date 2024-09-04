fd63c32a01261419fa44063810b2b91c0cd1b1c8
Michael Ritter <mike.ritter@lyrasis.org>
2024-09-04 14:02:24 -0600

    DRYD-1506: Add published related links

--- src/plugins/recordTypes/collectionobject/forms/photo.jsx
+++ src/plugins/recordTypes/collectionobject/forms/photo.jsx
@@ -235,6 +235,13 @@ const template = (configContext) => {
             <Field name="referenceNote" />
           </Field>
         </Field>
+
+        <Field name="publishedRelatedLinkGroupList">
+          <Field name="publishedRelatedLinkGroup">
+            <Field name="relatedLink" />
+            <Field name="descriptiveTitle" />
+          </Field>
+        </Field>
       </Panel>
 
       <Panel name="hierarchy" collapsible collapsed>
